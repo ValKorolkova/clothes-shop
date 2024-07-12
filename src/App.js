5b55b446e485329bae66ee699c94b4a65fdee772
Valeriya <lerakorolkova88@gmail.com>
2024-07-12 15:02:13 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Clothes from './components/ClothesComponents/Clothes';
+import Cart from './components/cart/Cart';
+import AllCategories from './components/filter/AllCaregories';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='mainContainer'>
+      <AllCategories/>
+      <Clothes />
+      </div>
+      <div className=' cart'>
+      <Cart />
+      </div>
     </div>
   );
 }
